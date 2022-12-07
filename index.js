8af569ede0fb064af96a4c6091668bcc3a01ecfd
prijacash <prijacash@gmail.com>
2022-12-07 14:50:11 -0800

    updated layout

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // required packages
-const fs = require('fs');
 const express = require('express');
 const layout = require('express-ejs-layouts');
+const fs = require('fs');
 
 // express app config
-const app = express()
+const app = express();
 const PORT = 3001
 app.set('view engine', 'ejs')
 app.use(layout)
@@ -12,14 +12,54 @@ app.use(layout)
 app.use(express.urlencoded({ extended: false }))
 
 // middleware
-app.use('/dinosaurs', require('/controllers/dinosaurs'))
-app.use('/prehistoric', require('/controllers/prehistoric_creatures'))
+// app.use('/dinosaurs', require('/controllers/dinosaurs'))
+// app.use('/prehistoric', require('/controllers/prehistoric_creatures'))
+
+// route definitions
+app.get('/', (req, res) => {
+  res.render('home.ejs')
+})
 
 // lists all dinosaurs
 app.get('/dinosaurs', (req, res) => {
     let dinosaurs = fs.readFileSync('./dinosaurs.json');
-    console.log(dinosaurs);
+    let dinoData = JSON.parse(dinosaurs);
+    res.render('dinosaurs/index', {myDinos: dinoData});
   });
+
+//express show route for dinosaurs (lists one dinosaur)
+app.get('/dinosaurs/:idx', (req, res) => {
+    // get dinosaurs
+    let dinosaurs = fs.readFileSync('./dinosaurs.json');
+    let dinoData = JSON.parse(dinosaurs);
+  
+    //get array index from url parameter
+    let dinoIndex = parseInt(req.params.idx);
+  
+    //render page with data of the specified animal
+    res.render('dinosaurs/show', {myDino: dinoData[dinoIndex]});
+  });
+
+// new route
+app.get('/dinosaurs/new', (req, res) => {
+    res.render('dinosaurs/new');
+  });
+
+// POST route
+app.post('/dinosaurs', (req, res) => {
+  // read dinosaurs file
+  let dinosaurs = fs.readFileSync('./dinosaurs.json');
+  let dinoData = JSON.parse(dinosaurs);
+
+  // add item to dinosaurs array
+  dinoData.push(req.body);
+
+  // save dinosaurs to the data.json file
+  fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData));
+
+  //redirect to the GET /dinosaurs route (index)
+  res.redirect('/dinosaurs');
+});
 
 // listen on a port
 app.listen(PORT, () => console.log(`is that dinos i hear ${PORT}🦕`))
